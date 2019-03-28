7d89349fb9c35b932da025354ad05760b08ca44e
toto240325 <toto240325@gmail.com>
2019-03-28 23:50:50 +0100

    sending email when starting (to detect that it has been stopped...)

--- getWindowTitle.ps1
+++ getWindowTitle.ps1
@@ -51,6 +51,48 @@ PRIMARY KEY (`fgw_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0;
 
 #>
+
+
+#------------------------------------------------------------------------------------
+
+if ($env:computername -eq "L02DI1453375DIT") {
+    $File = "C:\mydata\mytemp\b.txt"		
+    $filenameAndPath = $File
+}
+elseif ($env:computername -eq "HP2560") {
+    $File = "d:\temp\a.txt"
+}
+elseif ($env:computername -eq "MYPC3") {
+    $File = "d:\temp\d.txt"
+    $filenameAndPath = "d:\temp\att.txt"
+}
+else {
+    $File = "d:\temp\a.txt"
+    $filenameAndPath = "d:\temp\att.txt"
+}	
+function sendMail($emailTo, $subject, $body) {
+
+    $mailerUser = "mailer240325@gmail.com"
+    $mailerPw = "Mailer060502!h"
+
+    $secpasswd = ConvertTo-SecureString $mailerPw -AsPlainText -Force
+    $cred = New-Object -TypeName System.Management.Automation.PSCredential($mailerUser, $secpasswd)
+
+    $EmailFrom = "mailer240325@gmail.com"
+    $SMTPServer = "smtp.gmail.com" 
+    $SMTPMessage = New-Object System.Net.Mail.MailMessage($emailFrom, $emailTo, $subject, $body)
+    $attachment = New-Object System.Net.Mail.Attachment($filenameAndPath)
+    $SMTPMessage.Attachments.Add($attachment)
+    $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) 
+    $SMTPClient.EnableSsl = $true 
+    $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($cred.UserName, $cred.Password); 
+    $SMTPClient.Send($SMTPMessage)
+    $attachment.dispose()
+}	
+
+#sendMail "eric.derruine@gmail.com" "this is a test 4" "this is the body"
+
+
 #------------------------------------------------------------------------------------
 function ConnectMySQL([string]$user, [string]$pass, [string]$MySQLHost, [string]$database) {
     # Load MySQL .NET Connector Objects
@@ -778,7 +820,8 @@ $errorMsg | out-file -append -filepath $errorFile
 try {
     [bool]$global:wasCreated = $false;
     $mutant = New-Object System.Threading.Mutex($true, "MyMutexGetWindowTitle7", [ref] $global:wasCreated);        
-    if ($global:wasCreated) {            
+    if ($global:wasCreated) {     
+        sendMail "toto240325@gmail.com" "getWindowTitle starting at $dateTime" "this is the body"       
         mainJob;
     }
     else {
